4e73eb17a9dc224eb27912b9ad1f432f378970e3
Crowdbotics <team@crowdbotics.com>
2021-05-28 18:23:26 +0000

    Update models

--- backend/citizenshiphuf_27523/wsgi.py
+++ backend/citizenshiphuf_27523/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citizenshiphuf_27523.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "citizenshiphuf_27523.settings")
 
 application = get_wsgi_application()
